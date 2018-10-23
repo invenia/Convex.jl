052b653fc8cc5accd7a54563c48555db6063887c
Letif Mones <letif.mones@invenialabs.co.uk>
2018-10-23 17:30:37 +0100

    +(::Array{Float64,2}, ::Float64) fixed

--- src/dcp.jl
+++ src/dcp.jl
@@ -141,3 +141,7 @@ struct ComplexSign <: Sign                end
 *(s::ComplexSign, v::ConcaveVexity) = NotDcp()
 *(s::ComplexSign, v::NotDcp) = v
 *(v::Vexity, s::ComplexSign) = s*v
+
+# we need these for Julia 1.0
++(x::Array{Float64,2}, y::Float64) = x .+ y
++(x::Float64, y::Array{Float64,2}) = x .+ y
