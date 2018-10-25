07315609eb720db8ed8fcf087f508c2f1f1c360d
Letif Mones <letif.mones@invenialabs.co.uk>
2018-10-25 14:40:48 +0100

    reduce redundancy of hvcat

--- src/atoms/affine/stack.jl
+++ src/atoms/affine/stack.jl
@@ -120,16 +120,16 @@ vcat(args::AbstractExpr...) = transpose(HcatAtom(map(transpose, args)...))
 vcat(args::AbstractExprOrValue...) = transpose(HcatAtom(map(arg -> transpose(convert(AbstractExpr, arg)), args)...))
 vcat(args::Value...) = Base.cat(args..., dims=1) # Note: this makes general vcat slower for anyone using Convex...
 
-function hvcat(rows::Tuple{Vararg{Int}}, args::T...) where {T<:AbstractExpr}              
-    nbr = length(rows)                                                                       
-    rs = Vector{Any}(undef, nbr)                                                             
-    a = 1                                                                                    
-    for i = 1:nbr                                                                            
-        rs[i] = hcat(args[a:a-1+rows[i]]...)                                                 
-        a += rows[i]                                                                         
-    end                                                                                      
-    return vcat(rs...)                                                                       
-end
+#function hvcat(rows::Tuple{Vararg{Int}}, args::T...) where {T<:AbstractExpr}
+#    nbr = length(rows)
+#    rs = Vector{Any}(undef, nbr)
+#    a = 1
+#    for i = 1:nbr
+#        rs[i] = hcat(args[a:a-1+rows[i]]...)
+#        a += rows[i]
+#    end
+#    return vcat(rs...)
+#end
 function hvcat(rows::Tuple{Vararg{Int}}, args::AbstractExprOrValue...)
     nbr = length(rows)
     rs = Vector{Any}(undef, nbr)
